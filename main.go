a93197aa7b1b430da02b76b7809195a95e9613ac
Aditya Diwakar <aditya@diwakar.io>
2019-05-27 16:54:58 -0400

    Add templating engine

---
+++ main.go
@@ -0,0 +1,36 @@
+package templating
+
+import (
+	"io/ioutil"
+	"regexp"
+	"strings"
+)
+
+func check(e error) {
+    if e != nil {
+        panic(e)
+    }
+}
+
+func openTemplate(templateName string) []byte {
+	dat, err := ioutil.ReadFile("templates/" + templateName)
+    check(err)
+	return dat
+}
+
+func ReplaceOccurances(templateName string, items map[string]string) []byte {
+	re := regexp.MustCompile(`{{\s?[A-z]+\s?}}`)
+	file := openTemplate(templateName)
+	queries := re.FindAll(file, -1)
+
+	keywords := [][]byte{}
+	for  i := 0; i < len(queries); i++ { 
+		singleQuery := queries[i]
+		keyword := singleQuery[ 2 : len(singleQuery) - 2 ]
+		trimmedKeyword := strings.Trim(string(keyword), " ")
+		keywords = append(keywords, []byte(trimmedKeyword))
+
+		file = []byte(strings.Replace(string(file), string(singleQuery), string(items[trimmedKeyword]), -1))
+	}
+	return file
+}
